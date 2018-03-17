1de0343bdf54692ac2b147f7c199543f105325cb
M1mez <Fpscorner@gmail.com>
2018-03-17 12:42:54 +0100

    started prototype with wepaonMelee

---
+++ SWP/Functions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWP
+{
+    internal class Functions
+    {
+        private T DeepClone<T>(T obj)
+        {
+            T objResult;
+            using (var ms = new MemoryStream())
+            {
+                var bf = new BinaryFormatter();
+                bf.Serialize(ms, obj);
+                ms.Position = 0;
+                objResult = (T)bf.Deserialize(ms);
+            }
+            return objResult;
+        }
+    }
+}
---
+++ SWPTests/PrototypeTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SWP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWP.Tests
+{
+    [TestClass()]
+    public class PrototypeTests
+    {
+        [TestMethod()]
+        public void UnitTestTestTest()
+        {
+            var p = new Prototype();
+            Assert.AreEqual(1, p.UnitTestTest());
+        }
+    }
+}
